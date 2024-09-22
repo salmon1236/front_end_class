c3310970edd29ce15f98f98b3fd1a79dd35a6867
salmon1236 <fessong75@gmail.com>
2024-09-22 16:19:34 +0900

    !!

--- 00_homework/240921_7주차_1/js/script.js
+++ 00_homework/240921_7주차_1/js/script.js
@@ -1,20 +1,27 @@
 document.write("<h2>과제 1</h2>");
-document.write("<h2>Hi nice to meet you. my name is 신의철</h2>","<hr />");
+document.write("<h2>Hi nice to meet you. my name is 신의철</h2>", "<hr />");
 
 document.write("<h2>과제 2</h2>");
-var HwTxt_01 = ("올해의 연도는 ");
-var HwTxt_02 = ("년 입니다");
+var HwTxt_01 = "올해의 연도는 ";
+var HwTxt_02 = "년 입니다";
 var HwThisYear = 2024;
 var HwNextYear = Number(HwThisYear) - 3;
-document.write(HwTxt_01+HwNextYear+HwTxt_02,"<hr />");
+document.write(HwTxt_01 + HwNextYear + HwTxt_02, "<hr />");
 
 document.write("<h2>과제 3</h2>");
-var HwSnoopy_01 = "<img src='./img/snoopy_01.png' alt='스누피'class='snoopy_01'>"
-document.write(HwSnoopy_01,'<hr />');
+var HwSnoopy_01 =
+  "<img src='./img/snoopy_01.png' alt='스누피'class='snoopy_01'>";
+document.write(HwSnoopy_01, "<hr />");
 
 document.write("<h2>과제 4</h2>");
-var HwSnoopy_02 = "<div class='snoopy_02' style='background-image:url(./img/snoopy_02.png)'></div>"
+var HwSnoopy_02 =
+  "<div class='snoopy_02' style='background-image:url(./img/snoopy_02.png)'></div>";
 console.log(HwSnoopy_02);
 
-document.write(HwSnoopy_02,'<hr />');
+document.write(HwSnoopy_02, "<hr />");
 
+// 다음으로
+var button = "";
+button += "<div class='button-container'>";
+button += "<a href='../과제 2/index.html' class='button'>다음으로</a>";
+button += "</div>";
